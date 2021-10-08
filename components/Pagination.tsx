0af044a1ad9cde547635ef7f80be9a110eb8e17f
hiromu <hiromhiro617@gmail.com>
2021-10-08 11:03:08 +0900

    :lipstick: Pagination UI

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,23 +1,30 @@
 import { VFC } from "react";
 import Link from "next/link";
-import styled from "styled-components";
+import { useRouter } from "next/router";
+import styled, {css} from "styled-components";
 
 type Props = {
   totalCount: number;
   path: string;
 };
 
+type PaginationListItemProps = {
+  active: boolean
+}
+
 const PER_PAGE = 5;
 
-export const Pagination: VFC<Props> = ({ totalCount, path}) => {
-
+export const Pagination: VFC<Props> = ({ totalCount, path }) => {
+  const router = useRouter();
+  const { id } = router.query;
+  const currentPage: number = id ? +id : 1;
   const range = (start: number, end: number) =>
     [...Array(end - start + 1)].map((_, i) => start + i);
 
   return (
     <PaginationList>
       {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
-        <PaginationListItem key={index}>
+        <PaginationListItem key={index} active={currentPage === number}>
           <Link href={`/${path}/page/${number}`}>
             <a>{number}</a>
           </Link>
@@ -31,9 +38,21 @@ const PaginationList = styled.ul`
   width: 100%;
   display: flex;
   justify-content: center;
-  gap: 5px;
-`
+  gap: 10px;
+`;
 
-const PaginationListItem = styled.li`
+const PaginationListItem = styled.li<PaginationListItemProps>`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
   font-size: 1.25rem;
-`
+  border-radius: 100%;
+  text-align: center;
+  line-height: 50px;
+  ${(props) =>
+    props.active &&
+    css`
+      background: #856841;
+      color: white;
+    `}
+`;
